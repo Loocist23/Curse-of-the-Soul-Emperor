79cf3ca1646aab26e5f04045903a3e6804317fa6
Anthony <anthonyzegnal05@gmail.com>
2023-03-31 22:58:12 +0200

    implementtion of the enemy class

--- main.py
+++ main.py
@@ -25,6 +25,8 @@ class player:
     xp = 0
     xp_to_next_level = 100
     inventory = []
+    speciality = ""
+    competence_points = 0
 
     #player functions
     def __init__(self, name):
@@ -43,6 +45,9 @@ class player:
         self.defense += 10
         self.speed += 10
         self.xp_to_next_level += 100
+        self.competence_points += 1
+        print("You leveled up!")
+
     
     def add_xp(self, xp):
         self.xp += xp
@@ -68,10 +73,66 @@ class player:
         print("Speed: " + str(self.speed))
         print("Level: " + str(self.level))
         print("XP: " + str(self.xp) + "/" + str(self.xp_to_next_level))
+        print("Speciality: " + self.speciality)
+        print("Competence points: " + str(self.competence_points))
     
     def show_all(self):
         print("Your name is " + self.name)
         self.show_stats()
         self.show_inventory()
     
+
+########################################################################################################################################################
+
+#enemy class
+class enemy:
+    #enemy variables
+    name = "" #name of the enemy
+    health = 100 #health of the enemy
+    attack = 10 #attack of the enemy
+    defense = 10 #defense of the enemy
+    speed = 10 #speed of the enemy
+    level = 1 #level of the enemy
+    drop = [] #drop of the enemy
+
+    #enemy functions
+    def __init__(self, name, health, attack, defense, speed, level, drop):
+        self.name = name
+        self.health = health
+        self.attack = attack
+        self.defense = defense
+        self.speed = speed
+        self.level = level
+        self.drop = drop
     
+    def attack(self, player):
+        player.health -= self.attack
+    
+    def defend(self, player):
+        self.defense -= player.attack
+    
+    def level_up(self):
+        self.level += 1
+        self.health += 10
+        self.attack += 10
+        self.defense += 10
+        self.speed += 10
+    
+    def drop_item(self):
+        return self.drop[random.randint(0, len(self.drop) - 1)]
+    
+    def show_stats(self):
+        print("Stats:")
+        print("Health: " + str(self.health))
+        print("Attack: " + str(self.attack))
+        print("Defense: " + str(self.defense))
+        print("Speed: " + str(self.speed))
+        print("Level: " + str(self.level))
+    
+    def show_all(self):
+        print("Name: " + self.name)
+        self.show_stats()
+        print("Drop: " + self.drop_item())
+    
+
+########################################################################################################################################################
